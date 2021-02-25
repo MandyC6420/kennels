9f91c29a44b08c7b0b831d4a9fa18570a05e2f0c
Mandy <drgnfli6420@yahoo.com>
2021-02-25 14:52:11 -0500

    navbar complete

---
+++ src/components/PropsAndState.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+export const PropsAndState = ({ yourName }) => {
+
+  return (
+    <>
+      <h3>Welcome, {yourName} </h3>
+    </>
+  )
+}
+
+// import React, { useState } from "react"
+
+// export const PropsAndState = ({ yourName }) => {
+//   let [countClicks, setCountClicks] = useState(0)
+
+//   const handleClick = () => {
+//     //good practice:
+//     //make a copy of state, modifiy it, and then setState to the copy
+//     const newCountClicks = ++countClicks
+//     setCountClicks(newCountClicks)
+//   }
+
+//   return (
+//     <>
+//       <h3>Welcome, {yourName} </h3>
+//       <p>{countClicks}</p>
+//       <button onClick={(handleClick)}>Click Me</button>
+//     </>
+//   )
+// }
