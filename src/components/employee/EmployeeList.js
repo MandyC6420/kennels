d2d2f23106e0287965480d7347a555c75b8b963e
Mandy <drgnfli6420@yahoo.com>
2021-03-02 15:52:33 -0500

    add employees complete

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,29 +1,39 @@
-import React, { useContext, useEffect } from "react"
-import { EmployeeContext } from "./EmployeeProvider"
-import { EmployeeCard } from "./EmployeeCard"
-import "./Employee.css"
-
+import React, { useContext, useEffect } from "react";
+import { EmployeeContext } from "./EmployeeProvider";
+import { EmployeeCard } from "./EmployeeCard";
+import { getEmployees } from "./EmployeeProvider"
+import "./Employee.css";
+import { useHistory } from "react-router-dom"
+import { LocationContext } from "../location/LocationProvider"
 
 export const EmployeeList = () => {
-  // This state changes when `getAnimals()` is invoked below
-  const { employees, getEmployees } = useContext(EmployeeContext)
+  // This state changes when `getEmployees()` is invoked below
+  const { employees, getEmployees } = useContext(EmployeeContext);
+  const { locations, getLocations } = useContext(LocationContext)
 
+  const history = useHistory();
   //useEffect - reach out to the world for something
   useEffect(() => {
-    console.log("EmployeeList: useEffect - getEmployees")
-    getEmployees()
-
-  }, [])
-
+    console.log("EmployeeList: useEffect - getEmployees");
+    getEmployees();
+  }, []);
 
   return (
-    <div className="employees">
-      {console.log("EmployeeList: Render", employees)}
-      {
-        employees.map(employee => {
-          return <EmployeeCard key={employee.id} employee={employee} />
-        })
-      }
-    </div>
-  )
-}
+    <>
+      <h2>Employees</h2>
+      <button
+        onClick={() => {
+          history.push("/employees/create");
+        }}
+      >
+        Add Employee
+      </button>
+      <div className="employees">
+        {employees.map((employee) => {
+          const clinic = locations.find((l) => l.id === employee.locationId);
+          return (<EmployeeCard key={employee.id} employee={employee} />);
+        })}
+      </div>
+    </>
+  );
+};
