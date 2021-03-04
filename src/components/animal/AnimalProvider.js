65d6848c55bed71c545d3c4e60e31d8fe6eb47ef
Mandy <drgnfli6420@yahoo.com>
2021-03-04 15:32:29 -0500

    delete completed

--- src/components/animal/AnimalProvider.js
+++ src/components/animal/AnimalProvider.js
@@ -29,6 +29,13 @@ export const AnimalProvider = (props) => {
             .then(res => res.json())
     }
 
+    const releaseAnimal = animalId => {
+        return fetch(`http://localhost:8088/animals/${animalId}`, {
+            method: "DELETE"
+        })
+            .then(getAnimals)
+    }
+
     /*
         You return a context provider which has the
         `animals` state, `getAnimals` function,
@@ -37,9 +44,10 @@ export const AnimalProvider = (props) => {
     */
     return (
         <AnimalContext.Provider value={{
-            animals, addAnimal, getAnimals, getAnimalById
+            animals, addAnimal, getAnimals, getAnimalById, releaseAnimal
         }}>
             {props.children}
         </AnimalContext.Provider>
     )
 }
+
